0b5cce11f4f1705a31381fc90b0dd52c93fe8063
Reiko <haga4880@gmail.com>
2026-08-31 12:11:26 +0900

    スマホ対応用のviewport設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,21 @@
 const { API_KEY, SERVICE_DOMAIN } = process.env;
 
 export default defineNuxtConfig({
-  srcDir: 'src/',
-    runtimeConfig: {
-        apiKey: API_KEY,
-        serviceDomain: SERVICE_DOMAIN
+  srcDir: "src/",
+
+  // ▼ ここからスマホ対応のための設定を追加
+  app: {
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
     },
-    css: ['~/assets/css/reset.css', '~/assets/css/style.css',],
-  })
+  },
+  // ▲ ここまでを追加
+
+  runtimeConfig: {
+    apiKey: API_KEY,
+    serviceDomain: SERVICE_DOMAIN,
+  },
+  css: ["~/assets/css/reset.css", "~/assets/css/style.css"],
+});
